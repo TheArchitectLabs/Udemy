a0db05c4a4f8a19b442073bf740acdd102c47b41
The Architect <thearchitectlabs+github@gmail.com>
2022-12-25 07:43:31 -0500

    Initial project build

---
+++ Dive-Into-SpriteKit/FlyingThroughSpace/FlyingThroughSpace/GameScene.swift
@@ -0,0 +1,48 @@
+//
+//  GameScene.swift
+//  FlyingThroughSpace
+//
+//  Created by Michael & Diana Pascucci on 12/25/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
