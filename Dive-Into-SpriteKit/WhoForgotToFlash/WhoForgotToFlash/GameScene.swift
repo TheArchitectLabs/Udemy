f7f8fb295b9272b79190eac364523877a4755873
The Architect <thearchitectlabs+github@gmail.com>
2022-12-26 09:04:45 -0500

    Initial commit and setup files

---
+++ Dive-Into-SpriteKit/WhoForgotToFlash/WhoForgotToFlash/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  WhoForgotToFlash
+//
+//  Created by Michael & Diana Pascucci on 12/26/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // this method is called when your game scene is ready to run
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // this method is called when the user touches the screen
+
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // this method is called when the user stops touching the screen
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // this method is called before each frame is rendered
+       
+    }
+}
