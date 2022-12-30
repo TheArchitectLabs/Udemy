11180880767e2f1ea0364e3c822550e5a0f95e7e
The Architect <thearchitectlabs+github@gmail.com>
2022-12-30 07:30:33 -0500

    Initial commit of Crash Plane.

---
+++ Dive-Into-SpriteKit/CrashyPlane/CrashyPlane/GameScene.swift
@@ -0,0 +1,28 @@
+//
+//  GameScene.swift
+//  CrashyPlane
+//
+//  Created by Michael & Diana Pascucci on 12/30/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
